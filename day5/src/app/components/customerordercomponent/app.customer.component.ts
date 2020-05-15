a1c07cb13bf5c7de6d4eb0b2eaa36c384504ee4c
tavisca--mmehndiratta <mmehndiratta@affinionds.com>
2020-05-15 17:16:37 +0530

    bug fix for day5 assignment

--- day5/src/app/components/customerordercomponent/app.customer.component.ts
+++ day5/src/app/components/customerordercomponent/app.customer.component.ts
@@ -41,14 +41,17 @@ export class CustomerComponent implements OnInit {
     this.customer = new Customer(0,"","","",0);
     this._filteredCustomers = new Array<Customer>();
     this._customerIdList = new Array<number>();
+    console.log("search string value set inside constructor");
     this.searchString = "";
   }
 
   ngOnInit() { 
+    this.processFilteredData();
     this.serv.emitSearchString.subscribe((data) => {   
     this._customerIdList = new Array<number>();
-        console.log(`The search string value is ${data}`);
+        console.log(`The search textbox value is ${data}`);
         this.searchString = data;
+        this.processFilteredData();
     });
   }
 
@@ -59,19 +62,26 @@ export class CustomerComponent implements OnInit {
   }
 
   get getFilteredCustomers() : Array<Customer>{
+    return this._filteredCustomers;
+  }
+
+  processFilteredData() : void{
     this._filteredCustomers = this.customers.filter(cst => this.checkifDataExists(cst, this.searchString));
     if(this._filteredCustomers.length > 0)
     {
+      console.log("filtered customers are : " + JSON.stringify(this._filteredCustomers));
         for(let cst of this._filteredCustomers){
             console.log("customer id in filtered customers is : " + cst.CustomerId)
             this._customerIdList.push(cst.CustomerId);
         }
+        console.log("customer list value before emiting " + JSON.stringify(this._customerIdList));
         this.serv.onEmitCustomerIdList(this._customerIdList);
-        return this._filteredCustomers;
     }
-    return this.customers;
+    else{
+      this.serv.onEmitCustomerIdList(new Array<number>());      
+      this._filteredCustomers = this.customers;
+    }
   }
-
     checkifDataExists(cst: Customer, searchQuery: string) : boolean
     {
       if(searchQuery.length > 0)
